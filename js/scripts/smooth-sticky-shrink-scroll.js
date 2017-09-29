09748fd39ea691cd4cde33f224caed0cbf8bc7dd
Oleksandr Tsukur <tsukuros@gmail.com>
2017-09-30 00:35:46 +0300

    first iteration of side nav

--- js/scripts/smooth-sticky-shrink-scroll.js
+++ js/scripts/smooth-sticky-shrink-scroll.js
@@ -6,22 +6,26 @@
     this.$videoContainer = $('#hero-video-container');
     this.$heroSection = $('#hero');
     this.$stickyColumn = $('.sticky-column');
+    this.$header = $('.navbar.fixed-top');
     this.$win = $(window);
 
     this.updateDimensionsAndPositions();
     this.$win
       .on('scroll', this.onScroll.bind(this))
-      .on('resize', this.onResize.bind(this));
+      .on('resize', this.onResize.bind(this))
+      .scroll();
   }
 
   ShrinkableStickyHeroVideo.prototype = {
     constructor: ShrinkableStickyHeroVideo,
     onScroll: function(e) {
       var st = $(e.currentTarget).scrollTop();
+      var videoHeight = this.$video.outerHeight();
       if(st > 0) {
         this.$videoContainer.addClass('container');
         this.$video.addClass('in');
         var shrunkWidth = this.containerWidth - st;
+        // set video width
         if (shrunkWidth > this.stickyColumnWidth) {
           this.$video.css({
             width: shrunkWidth,
@@ -31,6 +35,12 @@
             width: this.stickyColumnWidth,
           });
         }
+        // pull video up if hit the bottom of the section
+        if (st + videoHeight > this.stickyColumnBottom) {
+          this.$video.css({ top: (this.stickyColumnBottom + this.headerHeight) - (st + this.headerHeight + videoHeight)})
+        } else {
+          this.$video.css({ top: '' });
+        }
       } else {
         this.$videoContainer.removeClass('container');
         this.$video.removeClass('in');
@@ -38,12 +48,15 @@
       }
     },
     updateDimensionsAndPositions: function() {
-      this.videoHeight = this.$video.height();
+      this.videoHeight = this.$video.outerHeight();
       this.stickyColumnWidth = this.$stickyColumn.width();
+      this.stickyColumnHeight = this.$stickyColumn.height();
+      this.stickyColumnBottom = this.$stickyColumn.offset().top + this.stickyColumnHeight;
       this.containerWidth = $('.container').first().width();
       this.bbox = this.$video[0].getBoundingClientRect();
       this.windowWidth = this.$win.width();
       this.videoRight = this.windowWidth - this.bbox.right;
+      this.headerHeight = this.$header.outerHeight();
 
       this.$video.css({right: this.videoRight});
       this.$heroSection.css({minHeight: this.videoHeight});
